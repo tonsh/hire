cb5baf473062b686fbb25522dc595e4b0a050e96
tonsh <tonshlee@gmail.com>
2014-07-09 23:26:47 +0800

    Fixed: 动态加载控制器

--- dongbeta/partII/index.php
+++ dongbeta/partII/index.php
@@ -1,6 +1,7 @@
 <?php
 // 项目目录
 define('PROJ_DIR', dirname(__FILE__));
+define("TPL_DIR", PROJ_DIR . '/templates');
 
 date_default_timezone_set('Asia/Shanghai');
 
@@ -11,10 +12,15 @@ require_once PROJ_DIR . '/libs/mysqli.db.php';
 
 require_once PROJ_DIR . '/models/base.php';
 require_once PROJ_DIR . '/models/stock.php';
-require_once PROJ_DIR . '/stock.ctl.php';
+require_once PROJ_DIR . '/controls/stock.php';
 
-# TODO 根据请求的控制器及方法动态执行
+# 根据请求的控制器及方法动态执行
+$ctl = isset($_GET['ctl']) ? strtolower($_GET['ctl']) : 'stock';
+$ctl = ucwords($ctl) . 'Controller';
 
-$obj = new StockController();
-$obj->on_view();
+$method = isset($_GET['mt']) ? strtolower($_GET['mt']) : 'index';
+$method = 'on_' . $method;
+
+$obj = new $ctl();
+$obj->$method();
 ?>
